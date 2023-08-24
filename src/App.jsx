899327d6a7acca11db54dfeaa51bebede6327fb1
Erick Tolentino <ericktolentino@pursuit.org>
2023-08-23 20:57:53 -0400

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-
+import Nav from '../components/Header'
+import.meta.env
+const URL = import.meta.env.VITE_BASE_API_URL;
 import './App.css'
 
-function App() {
 
- 
+
+function App() {
+  fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?key=${URL}`
+  ).then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  })
 
   return (
     <div>
-     <Header />
+     <Nav />
        
     </div>
 
