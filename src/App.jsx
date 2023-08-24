539739d069abc35df500a0d1d04116ad93b7d87e
Erick Tolentino <ericktolentino@pursuit.org>
2023-08-23 21:24:46 -0400

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Nav from '../components/Header'
 import.meta.env
-const URL = import.meta.env.VITE_BASE_API_URL;
+const API_KEY = import.meta.env.VITE_BASE_API_URL;
+const BASE_URL = 'https://youtube.googleapis.com/youtube/v3';
 import './App.css'
 
 
 
 function App() {
-  fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?key=${URL}`
-  ).then(response => response.json())
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      part: 'snippet',
+      q: 'coding', 
+      type: 'video',
+    });
+
+    fetch(`${BASE_URL}/search?${params}`)
+    .then(response => response.json())
   .then(data => {
-    console.log(data);
+    console.log(data.items)
+    setVideos(data.items)
   })
   .catch(error => {
     console.error('Error fetching data:', error);
   })
+}, []);
 
   return (
     <div>
-     <Nav />
+     <Nav etag={videos.etag}/>
        
     </div>
 
