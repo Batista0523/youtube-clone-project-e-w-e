7781ccf71b1d7af26784362b6533610c0d40a6ab
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-08-24 20:55:57 -0400

    import useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import RecommendVideo from "./components/RecommendVideo";
-import React from "react";
+
 
 import "./App.css";
 
