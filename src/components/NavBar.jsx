1a5f0272c98fa4d81da928fcce1f2c653003309a
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-08-25 19:03:15 -0400

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,15 @@
-
-
-import React from 'react';
-import './Nav.css';
-import '@fontsource/roboto/400.css';
+import React from "react";
+import "./Nav.css";
 
 const NavBar = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <i className="material-icons">menu</i>
 
-           <i className="material-icons">menu</i>
-
-            <h1>{video}</h1>
-            <input type="text" />
-         <button className='searchbtn'>search</button>
-
-        </div>
-    );
-}
+      <input type="text" />
+      <button className="searchbtn">search</button>
+    </div>
+  );
+};
 
 export default NavBar;
