7358cd9d454c15af89fb52c40203375bcfead79e
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-08-26 13:58:11 -0400

    added two className 1 for siderbar and one for app_page

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBar = () => {
     return (
-        <div>
+        <div className='sidebar'>
             <h1>this is the sidebar</h1>
          
         </div>
