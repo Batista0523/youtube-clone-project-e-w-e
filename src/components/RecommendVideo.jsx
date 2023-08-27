85a7b99f65e212151f180a91ef54560f5a70cfab
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-08-27 05:18:39 -0400

    added some css and also title tot eh video

--- src/components/RecommendVideo.jsx
+++ src/components/RecommendVideo.jsx
@@ -6,8 +6,9 @@ const RecommendVideo = ({ video }) => {
   return (
     <div className="video_preview">
         <p>{video.snippet.title}</p>
-        <p>{video.snippet.publishTime}</p>
       <img src={video.snippet.thumbnails.default.url} alt="Thumbnail" />
+        <p>{video.snippet.description}</p>
+        <p>{video.snippet.publishTime}</p>
     </div>
   );
 };
