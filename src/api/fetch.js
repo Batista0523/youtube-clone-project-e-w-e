49fc43b309ada5c2738248b441cec75faa909a50
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-08-27 14:06:18 -0400

    added

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,3 +3,9 @@ const BASE_URL = "https://youtube.googleapis.com/youtube/v3";
 export function getBaseUrl() {
   return BASE_URL;
 }
+
+async function fetchData() {
+    const responde = await fetch(URL);
+    const data = await responde.json();
+    console.log(responde)
+}
