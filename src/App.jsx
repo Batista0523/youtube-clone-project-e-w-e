c2c2df7e6e5a84c29f9b14e3e44beaa364fe86c7
Erick Tolentino <ericktolentino@pursuit.org>
2023-09-03 14:29:03 -0400

    fixed recommended videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
-import RecommendVideo from "./components/RecommendVideo";
 import ShowMore from "./components/ShowMore";
 import { getApiKey } from "./api/API";
-import "./App.css"
+import RecommendVideo from "./components/RecommendVideo"
+import "./App.css";
 
 function App() {
   const [recommendedVideos, setRecommendedVideos] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedVideoId, setSelectedVideoId] = useState(null);
 
   useEffect(() => {
     const apiKey = getApiKey();
@@ -27,27 +28,37 @@ function App() {
       });
   }, [searchQuery]);
 
+  const handleVideoClick = (videoId) => {
+    setSelectedVideoId(videoId);
+  };
+
   return (
     <div className="body">
-      <NavBar
-        setSearchQuery={setSearchQuery}
-        initialSearchQuery={searchQuery}
-      />
-      <div className="app_page" >
+      <NavBar className="header" setSearchQuery={setSearchQuery} initialSearchQuery={searchQuery} />
+      <div className="app_page">
         <SideBar className="side" />
         <div className="recommend">
           <h1>Recommend Video</h1>
-          {recommendedVideos.length > 0 ? (
-            recommendedVideos.map((video) => (
-              <RecommendVideo key={video.id.videoId} video={video} />
-            ))
-          ) : (
-            <p>No videos available</p>
-          )}
+          <section className="videos">
+            {recommendedVideos.length > 0 ? (
+              recommendedVideos.map((video) => (
+                <RecommendVideo
+                  key={video.id.videoId}
+                  video={video}
+                  onClick={() => handleVideoClick(video.id.videoId)}
+                />
+              ))
+            ) : (
+              <p>No videos available</p>
+            )}
+          </section>
         </div>
       </div>
+      {selectedVideoId && <ShowMore videoId={selectedVideoId} />}
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/RecommendVideo.jsx
+++ src/components/RecommendVideo.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Recommend.css"
-const RecommendVideo = ({ video }) => {
-  return (
-    <div className="recommend2">
-      <div className="video_preview">
+import "./Recommend.css";
+
+function RecommendVideo({ video, onClick }) {
+    return (
+      <div className="recommend2" onClick={onClick}>
+        <div className="video_preview">
           <p className="video-title">{video.snippet.title}</p>
-      
-        <img src={video.snippet.thumbnails.default.url} alt="Thumbnail" />
-        <p className="video-description">{video.snippet.description}</p>
-        <p className="video-publishTime">{video.snippet.publishTime}</p>
-        <p className="channelTitle">{video.snippet.channelTitle}</p>
+          <img src={video.snippet.thumbnails.default.url} alt="Thumbnail" />
+          <p className="video-description">{video.snippet.description}</p>
+          <p className="video-publishTime">{video.snippet.publishTime}</p>
+          <p className="channelTitle">{video.snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+  
 
 export default RecommendVideo;
