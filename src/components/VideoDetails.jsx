bc2016679cc8f494a36087089cd2d11b6ef2aa47
elisaul batista <elisaulbatista@pursuit.org>
2023-09-04 22:08:08 -0400

    added name and comment on video details

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const VideoDetails = () => {
   const { videoId } = useParams();
+
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    if (name.trim() === "" || comment.trim() === "") {
+      alert("Name and comment cannot be empty!");
+      return;
+    }
+
+    const newComment = { name, comment };
+    setComments([...comments, newComment]);
+    setName("");
+    setComment("");
+  };
 
   return (
     <div>
@@ -15,6 +39,34 @@ const VideoDetails = () => {
         frameBorder="0"
         allowFullScreen
       ></iframe>
+
+      <div>
+        <h2>Add a Comment</h2>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <input
+          type="text"
+          placeholder="Your Comment"
+          value={comment}
+          onChange={handleCommentChange}
+        />
+        <button onClick={handleCommentSubmit}>Submit</button>
+      </div>
+
+      <div>
+        <h2>Comments</h2>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>
+              <strong>{comment.name}:</strong> {comment.comment}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
